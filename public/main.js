d09379efad95f84ea3b39c7f5b2db4d6ee3fe742
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-06-14 16:30:34 -0400

    dumb appear animation

--- public/main.js
+++ public/main.js
@@ -46,6 +46,31 @@ function eeify_mathjs(){
     });
 }
 
+function appear(el,step){
+    max = 5;
+    if(step == undefined){
+	step = max;
+	el.style.position = "relative"
+    }
+    if(step < 0){
+	el.style.position = ""
+	return;
+    }
+
+    var ratio = step / max;
+    el.style.opacity = "0.0";
+    el.style.opacity = 1.0 - ratio;
+
+    el.style.left = -100 * ratio + "px";
+    console.log(ratio);
+    setTimeout(
+	function(){
+	    appear(el, step - 1);
+	},
+	33
+    );
+}
+
 function eecalc(root_el){
     eeify_mathjs();
     
@@ -65,7 +90,8 @@ function eecalc(root_el){
 	var input = subqsa(cell,".eecalc-input")[0];
 	var button = subqsa(cell,".eecalc-go-button")[0];
 	var output = subqsa(cell,".eecalc-output")[0];
-	
+
+	appear(cell);
 	input.focus();
 	
 	function calculate(){
