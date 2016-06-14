6abb199acf80fd569b500c37594d3cc7bad87587
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-06-14 15:07:24 -0400

    add more cells

--- public/main.js
+++ public/main.js
@@ -1,27 +1,56 @@
+/*
+  Shortcut to document.querySelectorAll
+ */
 function qsa(sel){
     return document.querySelectorAll(sel);
 }
 
+/*
+  qsa, but on an element
+ */
 function subqsa(el,sel){
     return el.querySelectorAll(sel);
 }
 
+/*
+  Load a template
+ */
 function load_template(name){
     var content = qsa("template[name="+name+"]")[0].innerHTML;
     return content;
 }
 
+/*
+  Create a dom element
+ */
+function new_el(html){
+    var node = document.createElement("div");
+    node.innerHTML = html;
+    return node.children[0];
+}
+
 function eecalc(root_el){
     root_el.innerHTML = load_template("eecalc");
+    var cells = subqsa(root_el,".eecalc-cells")[0];
+    new_eecalc_cell(cells);
+}
+
+function new_eecalc_cell(cells){
+    var cell = new_el(load_template("eecalc-cell"));
+    cells.appendChild(cell);
     
-    var input = subqsa(root_el,".eecalc-input")[0];
-    var button = subqsa(root_el,".eecalc-go-button")[0];
-    var output = subqsa(root_el,".eecalc-output")[0];
+    var input = subqsa(cell,".eecalc-input")[0];
+    var button = subqsa(cell,".eecalc-go-button")[0];
+    var output = subqsa(cell,".eecalc-output")[0];
+
+    input.focus();
     
     function calculate(){
 	var text = input.value;
 	var result = math.eval(text)
 	output.innerHTML = result;
+
+	new_eecalc_cell(cells);
     }
     
     input.onkeydown = function(e){
