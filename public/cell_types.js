9d6edd17760f97e74617561bf05c795d8688511a
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-08-06 17:24:10 -0400

    Dynamic button text

--- public/cell_types.js
+++ public/cell_types.js
@@ -13,19 +13,6 @@ var cell_types = {
             
             element.classList.add("less-detail");
                         
-            // Manage cell state (more / less details)
-            function switch_state(){
-                if(cell_state == "less-detail"){
-                    cell_state = "more-detail";
-                    element.classList.remove("less-detail");
-                    element.classList.add("more-detail");
-                } else {
-                    cell_state = "less-detail";
-                    element.classList.add("less-detail");
-                    element.classList.remove("more-detail");
-                }
-            }
-
             // Render content
             var content = render("mathjs-cell-type");
             extension_content.appendChild(content);
@@ -35,6 +22,21 @@ var cell_types = {
             var toggle_button = subqsa(element, ".toggle-detail")[0];
             console.log(subqsa(element, "*"));
             toggle_button.addEventListener("click", switch_state);
+
+            // Manage cell state (more / less details)
+            function switch_state(){
+                if(cell_state == "less-detail"){
+                    cell_state = "more-detail";
+                    element.classList.remove("less-detail");
+                    element.classList.add("more-detail");
+                    toggle_button.innerText = "Show less details";
+                } else {
+                    cell_state = "less-detail";
+                    element.classList.add("less-detail");
+                    element.classList.remove("more-detail");
+                    toggle_button.innerText = "Show more details";
+                }
+            }
 
         }
     },
