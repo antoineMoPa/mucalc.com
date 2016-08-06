64dddf801be5eb3f4020940626eac06b15e0a9fa
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-08-06 17:20:34 -0400

    toggle detail level button

--- public/cell_types.js
+++ public/cell_types.js
@@ -12,15 +12,30 @@ var cell_types = {
             var cell_state = "less-detail"
             
             element.classList.add("less-detail");
-            
+                        
             // Manage cell state (more / less details)
             function switch_state(){
-                
+                if(cell_state == "less-detail"){
+                    cell_state = "more-detail";
+                    element.classList.remove("less-detail");
+                    element.classList.add("more-detail");
+                } else {
+                    cell_state = "less-detail";
+                    element.classList.add("less-detail");
+                    element.classList.remove("more-detail");
+                }
             }
-            
+
+            // Render content
             var content = render("mathjs-cell-type");
-            
             extension_content.appendChild(content);
+
+
+            // Enable toggle detail button
+            var toggle_button = subqsa(element, ".toggle-detail")[0];
+            console.log(subqsa(element, "*"));
+            toggle_button.addEventListener("click", switch_state);
+
         }
     },
 }
